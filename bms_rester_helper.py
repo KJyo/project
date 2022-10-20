4679f3b01f44446d76637f4a65cdc0a5436b7305
KJyo <116248938+KJyo@users.noreply.github.com>
2022-10-20 16:41:08 +0530

    Update bms_rester_helper.py

--- bms_rester_helper.py
+++ bms_rester_helper.py
@@ -1,30 +1,30 @@
 import sandra
 import logging
-from App.config import Environment
+from project.config import Environment
 
 logger = logging.getLogger(__name__)
 
 class PermissionManager(object):
 
 	def __init__(self, config = None):
-		  self.__config = config or Environment()
+	    self.__config = config or Environment()
 		
 	@staticmethod
 	def user():
-		  user = Rest.current_user()
-		  if user == None:
-			    if platform.system() == 'windows':
-				  user = sandra.USER
-		  return user
+	    user = Rest.current_user()
+	    if user == None:
+	       if platform.system() == 'windows':
+		   user = sandra.USER
+            return user
 
 	@staticmethod
 	def logUserInfo(loginuser):
 		try:
-			  logger.info(" > %s  %s  %(request.method, request.url)) 
-			  if loginuser:
-				  logger.info('> User %' %loginuser)
-			    login.info('> User-Agent %' %(request.user_agent))
-			    login.info('> X-Client timeout:%s', request.header.get('X-Client-timeout', 'Not set'))
+		    logger.info(" > %s  %s  %(request.method, request.url)) 
+		    if loginuser:
+			 logger.info('> User %' %loginuser)
+			 login.info('> User-Agent %' %(request.user_agent))
+			 login.info('> X-Client timeout:%s', request.header.get('X-Client-timeout', 'Not set'))
 		except Exception as e:
 			logger.error('Error in authenticating the user '+ str(e))	
 
@@ -32,10 +32,10 @@ class PermissionManager(object):
 	@login_required(f):
 	@wraps(f)
 	def decorated_function(*args, **kwargs):
-			user = PermissionManager.currentUser()
-			PermissionManager.logUserInfo(user)
-				if user is None:
-					raise Exception('You are not authorized to access BMS. User id is None')
+	    user = PermissionManager.currentUser()
+	    PermissionManager.logUserInfo(user)
+	    if user is None:
+		raise Exception('You are not authorized to access BMS. User id is None')
 		    return f(args, kwargs)
 	    return decorated_fuction	
 
